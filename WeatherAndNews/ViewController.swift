f3c5ce06874486a5b7ff0467495195e4a22899c2
Cergei4213644576345t34r <30879405+Cergei4213644576345t34r@users.noreply.github.com>
2022-03-27 15:59:45 +0300

    Initial Commit

---
+++ WeatherAndNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAndNews
+//
+//  Created by Сергей Хотянович on 27.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
